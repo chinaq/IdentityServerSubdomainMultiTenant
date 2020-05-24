3b0a67dd50e6bc68bfdfc74a84a2152924c118a0
Lalita <git@lalitacode.com>
2020-05-24 15:45:36 +0545

    Retreive Authorization Urls In API

--- API/Helpers/TenantProvider.cs
+++ API/Helpers/TenantProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,22 @@ namespace API.Helpers
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public TenantProvider(IHttpContextAccessor httpContextAccessor)
-            => _httpContextAccessor = httpContextAccessor;
+        public IConfiguration Configuration { get; }
+
+        public TenantProvider(IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        { 
+            
+            _httpContextAccessor = httpContextAccessor;
+            Configuration = configuration;
+
+        }
 
         public string GetCurrentTenant()
         {
             string requestUrl = $"{this._httpContextAccessor.HttpContext.Request.Host}";
             var tenantId = requestUrl.Split('.')[0];
-            string authorityDomain = "lalita.com:5000";
-
-            string authorityScheme = "http";
+            string authorityDomain = Configuration.GetValue<string>("AuthorizationServerUrls:AuthorityDomain");
+            string authorityScheme = Configuration.GetValue<string>("AuthorizationServerUrls:AuthorityScheme");
             string authorityUrl = $"{authorityScheme}://{tenantId}.{authorityDomain}";
 
             return authorityUrl;
